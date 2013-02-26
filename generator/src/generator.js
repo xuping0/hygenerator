2636252e43e846ddef2f5cc2224c5d04e34ab51f
hopesfish <hopesfish@163.com>
2013-02-26 17:10:22 +0800

    1,支持global数据 2,支持按project生产html

--- generator/src/generator.js
+++ generator/src/generator.js
@@ -1,56 +1,80 @@
 var fs = require("fs");
 var doT = require("dot");
-var fs = require('fs'), configs = [];
+var fs = require('fs');
 
 var GLOABAL = {
-    TEMPLATES: "templates",
-    SOURCE: "web",
+    PROJECT: "project",
     OUTPUT: "../output"
 };
 
 function removeAll(path) {
     var dirList = fs.readdirSync(path);
     dirList.forEach(function(item) {
-        if (fs.statSync(path + '/' + item).isDirectory()) {
-            removeAll(path + '/' + item);
+        var filepath = path + '/' + item;
+        if (fs.statSync(filepath).isDirectory()) {
+            removeAll(filepath);
+            fs.rmdirSync(filepath);
         } else {
-            fs.unlinkSync(path + '/' + item);
+            fs.unlinkSync(filepath);
         }
     });
 }
 
-function walk(path) {
-    var dirList = fs.readdirSync(path);
+function initProjects(path, output) {
+    var dirList = fs.readdirSync(path), findPages, projects = [];
+
+    findPages = function(folder, pages) {
+        var dirList = fs.readdirSync(folder);
+        dirList.forEach(function(item) {
+            if (fs.statSync(folder + '/' + item).isDirectory()) {
+                findPages(folder + '/' + item);
+            } else {
+                pages.push(folder + '/' + item);
+            }
+        });
+    };
+    
     dirList.forEach(function(item) {
-        if (fs.statSync(path + '/' + item).isDirectory()) {
-            walk(path + '/' + item);
-        } else {
-            configs.push(path + '/' + item);
+        var projectPath = path + '/' + item;
+        if (fs.statSync(projectPath).isDirectory()) {
+            var project = {
+                name: item,
+                templates: projectPath + '/templates',
+                pages: [],
+                output: output + '/' + item,
+                global: JSON.parse(fs.readFileSync(projectPath + '/common.json').toString())
+            };
+            findPages(projectPath +  '/pages', project.pages);
+            projects.push(project);
         }
     });
+    return projects;
 }
+
 
 function generateHTML(file, template, data) {
     fs.writeFileSync(file, doT.template(template)(data));
 }
 
-function generateBasicHTML(path, config) {
+function generateBasicHTML(path, pageConfig, projectConfig) {
     // create the folder is not exsit
-    var paths = path.split("/");
+    var paths = path.split('/');
 
-    paths = paths.splice(1);
+    paths = paths.splice(3);
     paths = paths.splice(0, paths.length - 1);
 
-    var newdir = GLOABAL.OUTPUT + '/' + paths.join("/");
+    var newdir = projectConfig.output + '/' + paths.join('/');
+
     if (!fs.existsSync(newdir)) {
         fs.mkdirSync(newdir);
     }
 
     // get the template and the data
-    var template = fs.readFileSync(GLOABAL.TEMPLATES + '/' + config.template + '.html').toString();
+    var template = fs.readFileSync(projectConfig.templates + '/' + pageConfig.template + '.html').toString();
 
     // gennerate the html with the speicfic data
-    generateHTML(newdir + '/' + config.name + '.html', template, config.data);
+    pageConfig.data.global = projectConfig.global;
+    generateHTML(newdir + '/' + pageConfig.name + '.html', template, pageConfig.data);
 }
 
 function generateCascadeHTML(path, config) {
@@ -78,22 +102,33 @@ function generateCascadeHTML(path, config) {
     });
 }
 
-function generate() {
-    configs.forEach(function(path) {
-        var config = JSON.parse(fs.readFileSync(path).toString());
-        if (typeof config.template === "string") {
-            generateBasicHTML(path, config);
+function generateProject(project) {
+    // create the project folder in the output
+    if (!fs.existsSync(project.output)) {
+        fs.mkdirSync(project.output);
+    }
+    
+    // create the html base on the template and the page
+    project.pages.forEach(function(page) {
+        var pageConfig = JSON.parse(fs.readFileSync(page).toString());
+
+        if (typeof pageConfig.template === "string") {
+            generateBasicHTML(page, pageConfig, project);
         } else if (typeof config.template === "object") {
-            if (config.template.list && config.template.detail) {
-                generateCascadeHTML(path, config);
+            if (pageConfig.template.list && pageConfig.template.detail) {
+                generateCascadeHTML(pageConfig, project);
             }
         }
-        
+    });
+}
+
+function generate(projects) {
+    projects.forEach(function(project) {
+        generateProject(project);
     });
 }
 
 removeAll(GLOABAL.OUTPUT);
-walk(GLOABAL.SOURCE);
-generate();
+generate(initProjects(GLOABAL.PROJECT, GLOABAL.OUTPUT));
 
 
